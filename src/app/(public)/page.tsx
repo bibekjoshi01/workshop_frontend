70bd75844caf0f752dd8358bd9b978f73efeec5e
ManishJoc14 <manishjoc14@gmail.com>
2024-06-12 22:50:58 +0545

    hero with search

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/hero";
+import Search from "./components/search";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Search />
     </>
   );
 }
